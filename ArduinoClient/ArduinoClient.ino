c68db20a4717d61bd55aef7baf7e9cd2e7a90707
Bart Hertog <Bart@EmbeddedAMS.nl>
2022-06-14 11:02:13 +0200

    Connect to a wifi network.

--- ArduinoClient/ArduinoClient.ino
+++ ArduinoClient/ArduinoClient.ino
@@ -1,9 +1,144 @@
-void setup() {
-  // put your setup code here, to run once:
+/*
+ *  Copyright (C) 2020-2022 Embedded AMS B.V. - All Rights Reserved
+ *
+ *  This file is part of Embedded Proto.
+ *
+ *  Embedded Proto is open source software: you can redistribute it and/or 
+ *  modify it under the terms of the GNU General Public License as published 
+ *  by the Free Software Foundation, version 3 of the license.
+ *
+ *  Embedded Proto  is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Embedded Proto. If not, see <https://www.gnu.org/licenses/>.
+ *
+ *  For commercial and closed source application please visit:
+ *  <https://EmbeddedProto.com/license/>.
+ *
+ *  Embedded AMS B.V.
+ *  Info:
+ *    info at EmbeddedProto dot com
+ *
+ *  Postal address:
+ *    Johan Huizingalaan 763a
+ *    1066 VH, Amsterdam
+ *    the Netherlands
+ */
 
+#include <WiFi.h>
+
+// Please enter your configurations in the settings.h file:
+//  * WiFi name (SSID)
+//  * WiFi password
+//  * Server IP
+//  * Server Port number
+#include "settings.h" 
+
+// Setup global variables.
+WiFiClient client;
+
+// Use the Portenta RGB led to signal an error.
+void signal_error()
+{
+  digitalWrite(LEDR, LOW);
+  delay(100);
+  digitalWrite(LEDR, HIGH);
+  delay(100);
+  digitalWrite(LEDR, LOW);
+  delay(100);
+  digitalWrite(LEDR, HIGH);
+  delay(700);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+// Use the RGB led to signal we are not connected to wifi.
+void signal_wifi_disconnect()
+{
+  digitalWrite(LEDB, LOW);
+  delay(100);
+  digitalWrite(LEDB, HIGH);
+  delay(100);
+  digitalWrite(LEDB, LOW);
+  delay(100);
+  digitalWrite(LEDB, HIGH);
+  delay(700);
+}
 
+// Use the RGB led to signal all good.
+void signal_oke()
+{
+  digitalWrite(LEDG, LOW);
+  delay(500);
+  digitalWrite(LEDG, HIGH);
+  delay(500);
+}
+
+// Connect to WiFi.
+void connect_to_wifi()
+{
+  Serial.print("Connecting to SSID: ");
+  Serial.println(SECRET_SSID);
+  
+  auto status = WiFi.begin(SECRET_SSID, SECRET_PASS);
+  
+  while(WL_CONNECTED != status)
+  {
+    signal_wifi_disconnect();
+        
+    // wait 3 seconds for connection:
+    delay(3000);
+    
+    status = WiFi.begin(SECRET_SSID, SECRET_PASS);
+  }
+  
+  Serial.println("You're connected to the network");
+}
+
+void setup()
+{
+  // Initialize digital pin LED_BUILTIN as an output.
+  pinMode(LEDR, OUTPUT);
+  pinMode(LEDG, OUTPUT);
+  pinMode(LEDB, OUTPUT);
+
+  // Switch off the LEDs.
+  digitalWrite(LEDG, HIGH);
+  digitalWrite(LEDR, HIGH);
+  digitalWrite(LEDB, HIGH);
+  
+  // Initialize the serial port to output debug information.
+  Serial.begin(9600);
+  while(!Serial)
+  {
+    // Signal an error
+    signal_error();
+  }
+
+  connect_to_wifi();
+  
+  // Check for the WiFi module.
+  if(WL_NO_SHIELD == WiFi.status())
+  {
+    Serial.println("Communication with WiFi module failed!");
+    while (true) 
+    {
+      signal_error();
+    }
+  }
+}
+
+void loop() 
+{
+  if(WL_CONNECTED == WiFi.status())
+  {
+    signal_oke();
+
+    
+  }
+  else 
+  {
+    connect_to_wifi();
+  }
 }
